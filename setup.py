e85d5d0b4876ad822719d4414e6525c888f2383d
SimoneFardella <fardella93@gmail.com>
2015-12-16 09:59:34 +0100

    initial check-in

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(
+    name='escposprinter',
+    version='2.0',
+    url='https://github.com/Simonefardella/escposprinter',
+    download_url='https://github.com/Simonefardella/escposprinter/archive/master.zip',
+    description='Python library to manipulate ESC/POS Printers with support for python >= 3',
+    long_description=open('README.rst').read(),
+    license='LGPL',
+    author=['Simone Fardella'],
+    author_email=['fardella93@gmail.com'],
+    platforms=['linux'],
+    packages=[
+        'escposprinter',
+    ],
+    install_requires=[
+        'pyusb',
+        'Pillow>=2.0',
+        'qrcode>=4.0',
+        'pyserial',
+    ],
+)
