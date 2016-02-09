7c9d15f45e5b3c68412df3f2b401ea3a898a96f3
SimoneFardella <fardella93@gmail.com>
2016-02-09 15:49:47 +0100

    Managed SetupTools setup

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 setup(
     name='escposprinter',
-    version='2.5',
+    version='2.6',
     url='https://github.com/Simonefardella/escposprinter',
     download_url='https://github.com/Simonefardella/escposprinter/archive/master.zip',
     description='Python library to manipulate ESC/POS Printers with support for python >= 3',
