82cec8c5ca76ba19338853d59e0d096865472a3c
SimoneFardella <fardella93@gmail.com>
2016-04-13 23:33:34 +0200

    Minor bugfixes

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
 
 setup(
     name='escposprinter',
-    version='3.9',
+    version='4.0',
     url='https://github.com/Simonefardella/escposprinter',
     download_url='https://github.com/Simonefardella/escposprinter/archive/master.zip',
     description='Python library to manipulate ESC/POS Printers with support for python >= 3',
