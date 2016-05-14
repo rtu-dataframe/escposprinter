ebe02ab7011afe7dd7ff8ff6c91bbbf276057fef
Simone <fardella93@gmail.com>
2016-05-14 09:43:04 +0200

    Changes to printer resume procedure

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
 
 setup(
     name='escposprinter',
-    version='4.9',
+    version='5.0',
     url='https://github.com/Simonefardella/escposprinter',
     download_url='https://github.com/Simonefardella/escposprinter/archive/master.zip',
     description='Python library to manipulate ESC/POS Printers with support for python >= 3',
