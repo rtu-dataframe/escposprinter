dbf37c9a11ead7bb43f69e9aec9b69320c3a7e8b
simone <fardella93@gmail.com>
2017-07-07 19:58:52 +0200

    Minor changes: Better exception handling type on socket

--- AppTest.py
+++ AppTest.py
@@ -3,15 +3,15 @@ import datetime
 from escposprinter import *
 from escposprinter.escpos import EscposIO, Escpos
 
-printerAddress = '192.168.17.187'
+printerAddress = '192.168.17.189'
 printerPort = 9100
 
 string = str("String: " + str(datetime.datetime.now().microsecond))
-for iteration in range(1):
+for iteration in range(10):
     with EscposIO(printer.Network(printerAddress, printerPort)) as p:
         p.set(font='a', codepage='cp1251', size='normal', align='center', bold=True)
         p.writelines('LOGO')
-        p.printer.nvRamImage()
+        #p.printer.nvRamImage()
         #p.printer.image('IMAGE PATH')
         p.writelines('')
         p.writelines('')
@@ -20,15 +20,15 @@ for iteration in range(1):
 
 
 #OTHERWISE
-
-p = EscposIO(printer.Network(printerAddress, port=printerPort))
-p.autoCutMode = 'PAPER_PART_CUT'
-p.autoclose = False
-p.writelines('LOGO')
-p.printer.nvRamImage()
-p.writelines('')
-p.printer.cut()
-
+#
+# p = EscposIO(printer.Network(printerAddress, port=printerPort))
+# p.autoCutMode = 'PAPER_PART_CUT'
+# p.autoclose = False
+# p.writelines('LOGO')
+# p.printer.nvRamImage()
+# p.writelines('')
+# p.printer.cut()
+#
 
 
 
